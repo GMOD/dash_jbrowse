77779f037e239ae1e1a84be96f4d3cab01bddd20
Elliot Hershberg <eahershberg@gmail.com>
2021-08-25 16:00:43 -0700

    Clean up

--- src/lib/fragments/DashJbrowse.react.js
+++ src/lib/fragments/DashJbrowse.react.js
@@ -32,16 +32,3 @@ export default class DashJbrowse extends Component {
 
 DashJbrowse.defaultProps = defaultProps;
 DashJbrowse.propTypes = propTypes;
-
-/* 
-    onChange={
-        /*
-         * Send the new value to the parent component.
-         * setProps is a prop that is automatically supplied
-         * by dash's front-end ("dash-renderer").
-         * In a Dash app, this will update the component's
-         * props and send the data back to the Python Dash
-         * app server if a callback uses the modified prop as
-         * Input or State.
-         */
-// (e) => setProps({value: e.target.value})
