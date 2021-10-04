7c5c4c820eaf68354730a4d484c289254374be36
teresam856 <tere486martinez@gmail.com>
2021-10-04 14:57:23 -0700

    make sure tests run in headless mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.chrome.options import Options
 
 def pytest_setup_options():
+    """called before webdriver is initialized"""
     options = Options()
+    options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     return options
