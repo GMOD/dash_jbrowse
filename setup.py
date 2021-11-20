eed221f0a83ec7bf6994c4076da42dca939315dd
teresam856 <tere486martinez@gmail.com>
2021-11-20 03:05:02 -0800

    add links and project description to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 import json
 from setuptools import setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
+# read contents of package.json
 with open("package.json") as f:
     package = json.load(f)
 
@@ -18,15 +23,22 @@ setup(
     include_package_data=True,
     license=package["license"],
     description=package.get("description", package_name),
+    url=package["homepage"],
+    project_urls={
+        "Tracker": "https://github.com/GMOD/dash_jbrowse/issues",
+        "Source": "https://github.com/GMOD/dash_jbrowse/"
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         "dash>=1.15.0",
     ],
     classifiers=[
         "Framework :: Dash",
+        "Programming Language :: Python :: 3",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "Topic :: Scientific/Engineering :: Visualization",
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3",
         "Intended Audience :: Science/Research",
     ],
 )
