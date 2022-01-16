491b1cf9ee1304f42ebb1b2f9289cc996aa34864
teresam856 <tere486martinez@gmail.com>
2022-01-15 17:04:43 -0800

    upgrade react linear genome view package

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,6 @@ from selenium.webdriver.chrome.options import Options
 def pytest_setup_options():
     """called before webdriver is initialized"""
     options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
     return options
