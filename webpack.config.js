06b2842ebe8a058145ecdd732c1d49bd0f88cfca
Colin <colin.diesh@gmail.com>
2023-06-01 19:32:29 -0600

    Update build

--- webpack.config.js
+++ webpack.config.js
@@ -1,131 +1,50 @@
-const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
-const webpack = require('webpack');
-const WebpackDashDynamicImport = require('@plotly/webpack-dash-dynamic-import');
-const packagejson = require('./package.json');
+const path = require('path')
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+const webpack = require('webpack')
+const WebpackDashDynamicImport = require('@plotly/webpack-dash-dynamic-import')
+const packagejson = require('./package.json')
 
-const dashLibraryName = packagejson.name.replace(/-/g, '_');
+const dashLibraryName = packagejson.name.replace(/-/g, '_')
 
 module.exports = (env, argv) => {
-    let mode;
-
-    const overrides = module.exports || {};
-
-    // if user specified mode flag take that value
-    if (argv && argv.mode) {
-        mode = argv.mode;
-    }
-
-    // else if configuration object is already set (module.exports) use that value
-    else if (overrides.mode) {
-        mode = overrides.mode;
-    }
-
-    // else take webpack default (production)
-    else {
-        mode = 'production';
-    }
-
-    let filename = (overrides.output || {}).filename;
-    if (!filename) {
-        const modeSuffix = mode === 'development' ? 'dev' : 'min';
-        filename = `${dashLibraryName}.${modeSuffix}.js`;
-    }
-
-    const entry = overrides.entry || {main: './src/lib/index.js'};
-
-    const devtool = overrides.devtool || 'source-map';
-
-    const externals =
-        'externals' in overrides
-            ? overrides.externals
-            : {
-                  react: 'React',
-                  'react-dom': 'ReactDOM',
-                  'plotly.js': 'Plotly',
-                  'prop-types': 'PropTypes',
-              };
-
-    return {
-        mode,
-        entry,
-        output: {
-            path: path.resolve(__dirname, dashLibraryName),
-            chunkFilename: '[name].js',
-            filename,
-            library: dashLibraryName,
-            libraryTarget: 'window',
+  return {
+    mode: argv?.mode,
+    entry: { main: './src/lib/index.js' },
+    output: {
+      path: path.resolve(__dirname, dashLibraryName),
+      chunkFilename: '[name].js',
+      filename: `${dashLibraryName}.${
+        argv.mode === 'development' ? 'dev' : 'min'
+      }.js`,
+      library: dashLibraryName,
+      libraryTarget: 'window',
+    },
+    externals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      'plotly.js': 'Plotly',
+      'prop-types': 'PropTypes',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-        devtool,
-        externals,
-        module: {
-            rules: [
-                {
-                    test: /\.jsx?$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader',
-                    },
-                },
-                {
-                    test: /\.css$/,
-                    use: [
-                        {
-                            loader: 'style-loader',
-                            options: {
-                                insertAt: 'top',
-                            },
-                        },
-                        {
-                            loader: 'css-loader',
-                        },
-                    ],
-                },
-            ],
-        },
-        node: {
-            fs: 'empty',
-        },
-        optimization: {
-            minimizer: [
-                new TerserPlugin({
-                    sourceMap: true,
-                    parallel: true,
-                    cache: './.build_cache/terser',
-                    terserOptions: {
-                        warnings: false,
-                        ie8: false,
-                    },
-                }),
-            ],
-            splitChunks: {
-                name: true,
-                cacheGroups: {
-                    async: {
-                        chunks: 'async',
-                        minSize: 0,
-                        name(module, chunks, cacheGroupKey) {
-                            return `${cacheGroupKey}-${chunks[0].name}`;
-                        },
-                    },
-                    shared: {
-                        chunks: 'all',
-                        minSize: 0,
-                        minChunks: 2,
-                        name: 'dash_jbrowse-shared',
-                    },
-                },
-            },
-        },
-        plugins: [
-            new WebpackDashDynamicImport(),
-            new webpack.SourceMapDevToolPlugin({
-                filename: '[file].map',
-                exclude: ['async-plotlyjs'],
-            }),
-            new webpack.optimize.LimitChunkCountPlugin({
-                maxChunks: 1,
-            }),
-        ],
-    };
-};
+      ],
+    },
+
+    plugins: [
+      new WebpackDashDynamicImport(),
+      new NodePolyfillPlugin({
+        excludeAliases: ['console'],
+      }),
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1,
+      }),
+    ],
+  }
+}
