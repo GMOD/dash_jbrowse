8a576aebcd47b8df807551afb767582e178010b0
Colin <colin.diesh@gmail.com>
2023-12-11 11:34:37 -0500

    Misc

---
+++ R/RCircularGenomeView.R
@@ -0,0 +1,19 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+#' @export
+RCircularGenomeView <- function(id=NULL, aggregateTextSearchAdapters=NULL, assembly=NULL, configuration=NULL, defaultSession=NULL, tracks=NULL) {
+    
+    props <- list(id=id, aggregateTextSearchAdapters=aggregateTextSearchAdapters, assembly=assembly, configuration=configuration, defaultSession=defaultSession, tracks=tracks)
+    if (length(props) > 0) {
+        props <- props[!vapply(props, is.null, logical(1))]
+    }
+    component <- list(
+        props = props,
+        type = 'CircularGenomeView',
+        namespace = 'dash_jbrowse',
+        propNames = c('id', 'aggregateTextSearchAdapters', 'assembly', 'configuration', 'defaultSession', 'tracks'),
+        package = 'dashJbrowse'
+        )
+
+    structure(component, class = c('dash_component', 'list'))
+}
---
+++ src/DashJbrowse.jl
@@ -0,0 +1,65 @@
+
+module DashJbrowse
+using Dash
+
+const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
+const version = "1.3.4"
+
+include("jl/julia_circulargenomeview.jl")
+include("jl/julia_lineargenomeview.jl")
+
+function __init__()
+    DashBase.register_package(
+        DashBase.ResourcePkg(
+            "dash_jbrowse",
+            resources_path,
+            version = version,
+            [
+                DashBase.Resource(
+    relative_package_path = "async-LinearGenomeView.js",
+    external_url = "https://unpkg.com/dash_jbrowse@1.3.4/dash_jbrowse/async-LinearGenomeView.js",
+    dynamic = nothing,
+    async = :true,
+    type = :js
+),
+DashBase.Resource(
+    relative_package_path = "async-CircularGenomeView.js",
+    external_url = "https://unpkg.com/dash_jbrowse@1.3.4/dash_jbrowse/async-CircularGenomeView.js",
+    dynamic = nothing,
+    async = :true,
+    type = :js
+),
+DashBase.Resource(
+    relative_package_path = "async-LinearGenomeView.js.map",
+    external_url = "https://unpkg.com/dash_jbrowse@1.3.4/dash_jbrowse/async-LinearGenomeView.js.map",
+    dynamic = true,
+    async = nothing,
+    type = :js
+),
+DashBase.Resource(
+    relative_package_path = "async-CircularGenomeView.js.map",
+    external_url = "https://unpkg.com/dash_jbrowse@1.3.4/dash_jbrowse/async-CircularGenomeView.js.map",
+    dynamic = true,
+    async = nothing,
+    type = :js
+),
+DashBase.Resource(
+    relative_package_path = "dash_jbrowse.min.js",
+    external_url = "https://unpkg.com/dash_jbrowse@1.3.4/dash_jbrowse/dash_jbrowse.min.js",
+    dynamic = nothing,
+    async = nothing,
+    type = :js
+),
+DashBase.Resource(
+    relative_package_path = "dash_jbrowse.min.js.map",
+    external_url = "https://unpkg.com/dash_jbrowse@1.3.4/dash_jbrowse/dash_jbrowse.min.js.map",
+    dynamic = true,
+    async = nothing,
+    type = :js
+)
+            ]
+        )
+
+    )
+end
+end
